3e2cbd6c06f8e9e9f2b2f076a2d69e51c7b5ce15
Pilar Marinas <pilarmarinas@MBP-de-Pilar.local.lan>
2017-06-23 13:54:53 +0200

    Utils i directory

---
+++ src/base5/core/directory/subscribers.py
@@ -0,0 +1,34 @@
+from five import grok
+from plone import api
+from Products.PluggableAuthService.interfaces.authservice import IPropertiedUser
+from Products.PluggableAuthService.interfaces.events import IPrincipalCreatedEvent
+from Products.PluggableAuthService.interfaces.events import IPropertiesUpdatedEvent
+from Products.PluggableAuthService.interfaces.events import IUserLoggedInEvent
+
+
+from base5.core.utils import get_all_user_properties
+from base5.core.utils import add_user_to_catalog
+
+
+@grok.subscribe(IPropertiedUser, IPrincipalCreatedEvent)
+def create_user_hook(user, event):
+    """ This subscriber hooks on user creation and adds user properties to the
+        soup-based catalog for later searches
+    """
+    add_user_to_catalog(user)
+
+
+@grok.subscribe(IPropertiedUser, IPropertiesUpdatedEvent)
+def update_user_properties_hook(user, event):
+    """ This subscriber hooks on user creation and adds user properties to the
+        soup-based catalog for later searches
+    """
+
+    add_user_to_catalog(user, event.properties, overwrite=True)
+
+
+@grok.subscribe(IUserLoggedInEvent)
+def UpdateUserPropertiesOnLogin(event):
+    user = api.user.get_current()
+    properties = get_all_user_properties(user)
+    add_user_to_catalog(user, properties, overwrite=True)
