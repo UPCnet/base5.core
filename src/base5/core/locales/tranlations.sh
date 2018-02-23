a089a50d7a4356c06e9a6bd14dee05038a1ea61d
iago.lopez <iago.lopez@upcnet.es>
2018-02-23 11:47:01 +0100

    Traducciones

--- src/base5/core/locales/tranlations.sh
+++ src/base5/core/locales/tranlations.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-domain=base
+DOMAINS="base base5.portlets plone z3c.form"
+LANGUAGES="ca es en"
 
-msgfmt -o ca/LC_MESSAGES/$domain.mo  ca/LC_MESSAGES/$domain.po
-msgfmt -o es/LC_MESSAGES/$domain.mo  es/LC_MESSAGES/$domain.po
-msgfmt -o en/LC_MESSAGES/$domain.mo  en/LC_MESSAGES/$domain.po
-
-
-domainPortlets=base5.portlets
-
-msgfmt -o ca/LC_MESSAGES/$domainPortlets.mo  ca/LC_MESSAGES/$domainPortlets.po
-msgfmt -o es/LC_MESSAGES/$domainPortlets.mo  es/LC_MESSAGES/$domainPortlets.po
-msgfmt -o en/LC_MESSAGES/$domainPortlets.mo  en/LC_MESSAGES/$domainPortlets.po
+for domain in $DOMAINS
+do
+  for language in $LANGUAGES
+  do
+    if [ -f "$language/LC_MESSAGES/$domain.mo" ]
+    then
+      msgfmt -o $language/LC_MESSAGES/$domain.mo  $language/LC_MESSAGES/$domain.po
+    fi
+  done
+done
