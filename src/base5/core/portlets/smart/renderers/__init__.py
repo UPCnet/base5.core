de0892da6d7936de69e4a21335c0c7293034278a
iago.lopez <iago.lopez@upcnet.es>
2018-07-16 09:54:59 +0200

    Portlet Smart

---
+++ src/base5/core/portlets/smart/renderers/__init__.py
@@ -0,0 +1,31 @@
+class PortletItemRenderer(object):
+
+    def __init__(self, context):
+        self.item = context
+
+    def __call__(self, portletrenderer, **kwargs):
+        self.portlet = portletrenderer
+        self.request = portletrenderer.request
+        self.context = portletrenderer.context
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+        return {'css_class': self.css_class,
+                'html': self.template(self)}
+
+
+class PortletContainerRenderer(object):
+
+    def __init__(self, context):
+        self.portlet = context
+        self.context = self.portlet.context
+        self.request = self.portlet.request
+
+    def __call__(self, **kwargs):
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+        return self.template(self)
+
