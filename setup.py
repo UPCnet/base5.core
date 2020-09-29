052f8d3c3910eb7eb9b4773039ff757ac5f3fce0
pilar.marinas <pilar.marinas@upcnet.es>
2020-09-29 12:35:04 +0200

    Back to development: 0.39

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='base5.core',
-    version='0.38',
+    version='0.39.dev0',
     description="Base Genweb, Ulearn, etc Plone 5",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
