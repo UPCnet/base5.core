d27395b95271c6460d935da9dc061ae3a7da0f45
Iago López <ilopezsmx2011@gmail.com>
2024-12-12 16:03:23 +0100

    Remove grok

--- src/base5/core/browser/setup.py
+++ src/base5/core/browser/setup.py
@@ -530,6 +530,7 @@ If the most preferent plugin is:
 
         return 'Done'
 
+
 class UserMaxNotLDAP(BrowserView):
     """ Users in MAX not in LDAP.
         Vista per veure quins usuaris estan en el MAX i no al LDAP abans esborrar
@@ -589,6 +590,7 @@ class UserMaxNotLDAP(BrowserView):
             logger.info('The order to the plugins in En ACL_USERS / LDAP / Properties / Active Plugins : mutable_properties / auto_group / ldapaspb')
             results.append('The order to the plugins in En ACL_USERS / LDAP / Properties / Active Plugins : mutable_properties / auto_group / ldapaspb')
             return 'Error: ' + '\n'.join([str(item) for item in results])
+
 
 class DeleteUserMaxNotLDAP(BrowserView):
     """ Delete users in MAX not in LDAP.
@@ -697,6 +699,7 @@ class DeleteUserMaxNotLDAP(BrowserView):
             results.append('The order to the plugins in En ACL_USERS / LDAP / Properties / Active Plugins : mutable_properties / auto_group / ldapaspb')
             return 'Error: ' + '\n'.join([str(item) for item in results])
 
+
 class DeleteUserPropertiesCatalog(BrowserView):
     """ Delete users in catalog not in LDAP.
         Vista per esborrar del cataleg i que no apareguin al directori
@@ -783,6 +786,7 @@ class delete_local_roles(BrowserView):
 
         return 'Done'
 
+
 class users_to_delete_local_roles(BrowserView):
     """ Users to delete local roles of specified members.
         Vista per veure els usuaris que estan esborrats pero pendents esborrar permisos site
@@ -814,6 +818,7 @@ class users_to_delete_local_roles(BrowserView):
             logger.info('Except Users to delete local roles')
             results.append('Except Users to delete local roles')
             return 'Error: ' + '\n'.join([str(item) for item in results])
+
 
 class rebuild_users_portrait(BrowserView):
     """ Users portrait
@@ -911,121 +916,3 @@ class view_users_portrait(BrowserView):
             result[record[1].attrs['id_username']] = item
 
         return result
-
-
-
-
-
-
-
-# -*- coding: utf-8 -*-
-from Products.CMFPlone.interfaces import IPloneSiteRoot
-from Products.PluggableAuthService.interfaces.plugins import IPropertiesPlugin
-
-from five import grok
-from plone import api
-from souper.soup import get_soup
-from zope.interface import alsoProvides
-
-from base5.core.utils import add_user_to_catalog,remove_user_from_catalog
-
-grok.templatedir('views_templates')
-
-
-class addUserPropertiesCatalog(grok.View):
-    """ Rebuild the OMEGA13 repoze.catalog for specific user properties data
-
-        For default, we use the mutable_properties (users who have entered into communities)
-
-        Path directo del plugin:
-        acl_users/plugins/manage_plugins?plugin_type=IPropertiesPlugin
-
-        En ACL_USERS / LDAP / Properties / Active Plugins ha de estar ordenado así:
-          mutable_properties / auto_group / ldapaspb
-
-        But really, we use the most preferent plugin
-        If the most preferent plugin is:
-           mutable_properties --> users who have entered into communities
-           ldap --> users in LDAP
-    """
-    grok.context(IPloneSiteRoot)
-    grok.name('add_user_catalog')
-    grok.template('add_user_catalog')
-    grok.require('base.webmaster')
-
-    def update(self):
-        try:
-            from plone.protect.interfaces import IDisableCSRFProtection
-            alsoProvides(self.request, IDisableCSRFProtection)
-        except:
-            pass
-
-        if 'users' in self.request.form:
-            portal = api.portal.get()
-            plugins = portal.acl_users.plugins.listPlugins(IPropertiesPlugin)
-
-            # We use the most preferent plugin
-            # If the most preferent plugin is:
-            #    mutable_properties --> users who have entered into communities
-            #    ldap --> users in LDAP
-            pplugin = plugins[-1][1]
-
-            usersid = self.request.form['users'].split(',')
-            msg = ''
-            for userid in usersid:
-                user_properties = pplugin.enumerateUsers(id=userid)
-                for user in user_properties:
-                    if user['id'] == userid:
-                        user.update(dict(username=user['id']))
-                        if 'title' in user:
-                            user.update(dict(fullname=user['title']))
-                        elif 'fullname' in user:
-                            user.update(dict(fullname=user['fullname']))
-                        elif 'sn' in user:
-                            user.update(dict(fullname=user['sn']))
-                        else:
-                            user.update(dict(fullname=user['cn']))
-
-                        user_obj = api.user.get(user['id'])
-
-                        if user_obj:
-                            add_user_to_catalog(user_obj, user)
-                            print('Updated properties catalog for ' + user['id'])
-                            msg += user['id'] + ': Ok\n'
-
-                        else:
-                            print('No user found in user repository (LDAP) ' + user['id'])
-                            msg += user['id'] + ': Error\n'
-
-            print('Finish add_user_catalog')
-            if msg != '':
-                self.context.plone_utils.addPortalMessage(msg, 'info')
-
-
-class removeUserPropertiesCatalog(grok.View):
-    """ Remove specific user in catalog.
-    """
-    grok.context(IPloneSiteRoot)
-    grok.name('remove_user_catalog')
-    grok.template('remove_user_catalog')
-    grok.require('cmf.ManagePortal')
-
-    def update(self):
-        try:
-            from plone.protect.interfaces import IDisableCSRFProtection
-            alsoProvides(self.request, IDisableCSRFProtection)
-        except:
-            pass
-
-        if 'users' in self.request.form:
-            users = self.request.form['users'].split(',')
-
-            msg = ''
-            for username in users:
-                remove_user_from_catalog(username)
-                print('Delete properties catalog for ' + username)
-                msg += username + ': Ok\n'
-
-            print('Finish remove_user_catalog')
-            if msg != '':
-                self.context.plone_utils.addPortalMessage(msg, 'info')
